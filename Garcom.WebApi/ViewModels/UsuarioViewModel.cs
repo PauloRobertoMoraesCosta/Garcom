b48958d364b199441bdd06d86ed66aa9a9177463
Vitor A. Vale <vitoravale@outlook.com>
2016-05-05 20:12:20 -0300

    add usuarioviewmodel

---
+++ Garcom.WebApi/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garcom.WebApi.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Ativo { get; set; }
+    }
+}
